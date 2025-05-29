7e32305cc491189c0c61d87bd423d36618f375fa
Maksim Tarasenko <keltullis43@gmail.com>
2025-05-29 22:21:31 +0300

    I have created a new logger module and successfully configured the logger format for Hypercorn.

--- source/app.py
+++ source/app.py
@@ -1,69 +1,20 @@
 import importlib.metadata
-import logging
 import os
-import sys
 
 import fastapi
-import starlette.requests
 from fastapi.middleware.cors import CORSMiddleware
 from loguru import logger
 from setproctitle import setproctitle
 
 from source.core.database.postgres import check_db_connection
+from source.endpoints import router
+from source.exceptions.api import exception_handlers
+from source.logger import logging_dependency, set_logging
 from source.settings import settings
-
-from .endpoints import router
-from .exceptions.api import exception_handlers
 
 _app = None
 
 VERSION = importlib.metadata.version("source")
-
-
-async def logging_dependency(request: starlette.requests.Request):
-    params = [f"{name}: {value}" for name, value in request.path_params.items()]
-    headers = [f"{name}: {value}" for name, value in request.headers.items()]
-    logger.debug(f"{request.method} {request.url}")
-    logger.debug(f"Params: {params}")
-    logger.debug(f"Headers: {headers}")
-
-
-class InterceptHandler(logging.Handler):
-    def emit(self, record):
-        try:
-            level = logger.level(record.levelname).name
-        except ValueError:
-            level = record.levelno
-
-        frame, depth = logging.currentframe(), 2
-        while frame.f_code.co_filename == logging.__file__:
-            frame = frame.f_back
-            depth += 1
-
-        logger.opt(depth=depth, exception=record.exc_info).log(level, record.getMessage())
-
-
-def set_logging():  # sourcery skip: avoid-builtin-shadow
-    intercept_handler = InterceptHandler()
-    logging.root.setLevel(settings.log_level)
-    seen = set()
-    for name in [
-        *logging.root.manager.loggerDict.keys(),
-        "gunicorn",
-        "gunicorn.access",
-        "gunicorn.error",
-    ]:
-        if name not in seen:
-            seen.add(name.split(".")[0])
-            logging.getLogger(name).handlers = [intercept_handler]
-
-    _format = (
-        "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | "
-        "<level>{level: <8}</level> | {process} |"
-        "<cyan>{name}</cyan>:<cyan>{function}</cyan>:"
-        "<cyan>{line}</cyan> - <level>{message}</level>"
-    )
-    logger.configure(handlers=[{"sink": sys.stdout, "serialize": 0, "format": _format}])
 
 
 def create_app():
--- source/asgi.py
+++ source/asgi.py
@@ -1,80 +1,22 @@
 import asyncio
-import logging
-import signal
-import sys
 
-import uvicorn.server
 from loguru import logger
-
 from hypercorn.asyncio import serve
 from hypercorn.config import Config
 
 from source import app
 
-from . import settings
-
-
-class InterceptHandler(logging.Handler):
-    def emit(self, record):
-        try:
-            level = logger.level(record.levelname).name
-        except ValueError:
-            level = record.levelno
-
-        frame, depth = logging.currentframe(), 2
-        while frame.f_code.co_filename == logging.__file__:
-            frame = frame.f_back
-            depth += 1
-
-        logger.opt(depth=depth, exception=record.exc_info).log(level, record.getMessage())
-
-
-def setup_logging(level="DEBUG"):
-    # Устанавливаем уровень root логгера
-    logging.root.handlers = []
-    logging.root.setLevel(level)
-
-    intercept_handler = InterceptHandler()
-
-    targets = [
-        "uvicorn",
-        "uvicorn.access",
-        "uvicorn.error",
-        "hypercorn",
-        "hypercorn.access",
-        "hypercorn.error",
-        "asyncio",
-    ]
-
-    for name in targets:
-        logging_logger = logging.getLogger(name)
-        logging_logger.handlers = [intercept_handler]
-        logging_logger.propagate = False  # чтобы не было дублей
-        logging_logger.setLevel(level)
-
-    logger.configure(
-        handlers=[{
-            "sink": sys.stdout,
-            "format": (
-                "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | "
-                "<level>{level: <8}</level> | {process} | "
-                "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - "
-                "<level>{message}</level>"
-            ),
-            "level": level
-        }]
-    )
-
+from source import settings
+from source.logger import AsyncLoguruLogger
 
 
 def run():
-    setup_logging(settings.log_level.upper())
-
     config = Config()
     config.bind = [f"{settings.api.host}:{settings.api.port}"]
     config.workers = settings.workers
     config.loglevel = settings.log_level.lower()
     config.use_reloader = settings.debug
+    config.logger_class = AsyncLoguruLogger
     config.errorlog = "-"
     config.accesslog = "-"
 
