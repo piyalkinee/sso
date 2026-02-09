164a0b72ec44a00fbab2fafe9a18fd738ef43e80
Edward <47050768+piyalkinee@users.noreply.github.com>
2026-02-09 23:30:37 +0500

    Update

--- source/schemas/middleware.py
+++ source/schemas/middleware.py
@@ -4,6 +4,6 @@ from databases.core import Connection
 
 class Session:
     def __init__(self, request=None):
-        request: Request = request
-        db: Connection
-        user = None  # Explicitly allow user attribute
+        self.request: Request = request
+        self.db: Connection = None
+        self.user = None
