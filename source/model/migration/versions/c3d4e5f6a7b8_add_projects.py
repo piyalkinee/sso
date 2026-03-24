7e48402f50680117d846271f799cbdd4dcaea56f
Edward <47050768+piyalkinee@users.noreply.github.com>
2026-03-25 01:57:42 +0500

    Add project system

--- source/model/migration/versions/c3d4e5f6a7b8_add_projects.py
+++ source/model/migration/versions/c3d4e5f6a7b8_add_projects.py
@@ -17,7 +17,7 @@ depends_on: Union[str, Sequence[str], None] = None
 
 def upgrade() -> None:
     op.execute('CREATE EXTENSION IF NOT EXISTS "pgcrypto"')
-    op.execute('CREATE SCHEMA projects')
+    op.execute('CREATE SCHEMA IF NOT EXISTS projects')
     op.create_table(
         'projects_core',
         sa.Column('id', sa.UUID(), server_default=sa.text('gen_random_uuid()'), nullable=False),
